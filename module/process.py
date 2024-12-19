b8fb21fba773bbffa6c0f9b7a8ca147676a646e0
Nick <kkkygsos@naver.com>
2024-12-19 10:03:19 +0900

    테스트 코드 수정
    테스트 코드 수정

--- module/process.py
+++ module/process.py
@@ -100,20 +100,29 @@ if __name__ == "__main__":
         """
         log_manager.logger.info("로그인 자동화 프로세스를 시작합니다.")
 
-        # 단계별 설정 (단계 이름, 이미지 파일명, 더블클릭 여부, 대기 시간)
+        # 단계별 설정 (단계 이름, 이미지 파일명 또는 좌표, 더블클릭 여부, 대기 시간, 드래그 설정)
         steps = [
             {"step": "1단계: 로그인 아이콘 클릭", "image": "a_icon.png", "double_click": True, "wait": 8},
             {"step": "2단계: 구글 로그인", "image": "b_google.png", "wait": 3},
-            {"step": "3단계: 아이디 로그인", "image": "c_google_login.png", "wait": 3},
+            {"step": "3단계: 아이디 클릭", "image": (500, 300), "wait": 3},
             {"step": "4단계: 계속", "image": "d_keep_going.png", "wait": 5},
             {"step": "5단계: 페이지 닫기", "image": "e_closepage.png", "wait": 3},
             {"step": "6단계: 웹페이지 종료", "image": "f_exit.png", "wait": 10},
             {"step": "7단계: 게임 시작", "image": "g_gamestart.png", "wait": 3}
         ]
 
+        process_step = ProcessStep(base_path=path_manager.get_path("assets_login"))
+
         # 각 단계 실행
         for step in steps:
-            if not ProcessStep.execute(step["step"], step["image"], step.get("double_click", False), step["wait"]):
+            if not process_step.execute(
+                step["step"], 
+                step["image"], 
+                step.get("double_click", False), 
+                step.get("drag"),
+                step.get("window_name"),
+                step.get("wait", 3)
+            ):
                 return  # 단계 실패 시 종료
 
         # 추가 동작: 게임 창 포커스 및 크기 조정
