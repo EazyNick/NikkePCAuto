009ba397479a27f9e43caf65c1e40fa32b54a35c
Nick <kkkygsos@naver.com>
2024-12-23 10:56:54 +0900

    readme수정
    readme수정

--- V000_main.py
+++ V000_main.py
@@ -80,13 +80,13 @@ if __name__ == "__main__":
     automation = NikkeAutomation()
 
     # 커맨드 등록
-    # automation.register_command("login", LoginCommand())
-    # automation.register_command("dailycheck", DailycheckCommand())
-    # automation.register_command("freinds", FreindCommand())
-    # automation.register_command("outpost", OutpostCommand())    
-    # automation.register_command("shop", ShopCommand())
-    # automation.register_command("mail", MailCommand())
-    # automation.register_command("ark", ArkCommand())
+    automation.register_command("login", LoginCommand())
+    automation.register_command("dailycheck", DailycheckCommand())
+    automation.register_command("freinds", FreindCommand())
+    automation.register_command("outpost", OutpostCommand())    
+    automation.register_command("shop", ShopCommand())
+    automation.register_command("mail", MailCommand())
+    automation.register_command("ark", ArkCommand())
     automation.register_command("favorite_run", Favoritecommand())
 
     # 자동화 시작
